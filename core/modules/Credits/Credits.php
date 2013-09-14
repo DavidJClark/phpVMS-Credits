b39f84b0f131a8ea66e447af2e3d5456018372fa
DavidJClark <david@noticeabledesigns.com>
2013-09-14 11:25:08 -0400

    Added function to rename page title to "Partners"

--- core/modules/Credits/Credits.php
+++ core/modules/Credits/Credits.php
@@ -3,6 +3,7 @@
 class credits extends CodonModule   {
     
     function index()    {
+        $this->title = 'Partners';
         $this->set('credits', CreditsData::get_active_credits());
         $this->show('credits/index');
     }
